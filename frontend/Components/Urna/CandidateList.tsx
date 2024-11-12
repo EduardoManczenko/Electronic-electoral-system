4ea3d0f5a2153ed2342f3c2f2f2040775cb0fa73
Eduardo <112199620+EduardoManczenko@users.noreply.github.com>
2024-11-12 00:12:20 -0300

    feat: scroll down candidate list

--- frontend/Components/Urna/CandidateList.tsx
+++ frontend/Components/Urna/CandidateList.tsx
@@ -1,14 +1,21 @@
 import CandidateListCard from './CandidateListCard';
 
+export default function CandidateList() {
+    return (
+        <div className="flex flex-col w-full h-full gap-1 overflow-y-scroll">
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
+            <CandidateListCard />
 
-export default function CandidateList(){
-    return(
-        <div className="flex flex-col w-full h-full gap-1">
-            <CandidateListCard/>
-            <CandidateListCard/>
-            <CandidateListCard/>
-            <CandidateListCard/>
-    
         </div>
-    )
+    );
 }
--- frontend/Components/Urna/CandidateListCard.tsx
+++ frontend/Components/Urna/CandidateListCard.tsx
@@ -2,19 +2,20 @@ import Image from 'next/image';
 
 export default function CandidateListCard(){
     return(
-        <div className="bg-[#c9f0cb] w-full flex justify-center rounded-md gap-4 h-[25%]">
-            <div className='w-[40%] flex jusfity-center'>
+        <div className="bg-[#c9f0cb] w-full flex justify-center rounded-md gap-4 h-[30%]">
+
+            <div className='w-[30%] flex jusfity-center'>
                 <Image width={110} height={120} src="/images/bozonaro.png" alt="justica_eleitoral" className='rounded-md border-green-100 border-2 w-full h-full'></Image>
             </div>
 
-            <div className='flex flex-col justify-center gap-1 font-bold w-[40%]'>
+            <div className='flex flex-col items-center justify-center gap-1 font-bold w-[20%] text-lg font-bold'>
                 <div>Jair Bolsonaro</div>
                 <div>PT</div>
                 <div>22</div>
             </div>
 
-            <div className='flex flex-col justify-center pr-2 w-[80%]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iure obcaecati esse officiis nemo atque eligendi lorem
+            <div className='flex flex-col text-lg justify-center pr-2 w-[80%]'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iure obcaecati esse officiis nemo atque eligendi lorem, Nesciunt iure obcaecati esse officiis nemo atque eligendi lorem, Nesciunt iure obcaecati esse officiis nemo atque eligendi lorem, Nesciunt iure obcaecati esse officiis nemo atque eligendi lorem
             </div>
     </div>
     )
