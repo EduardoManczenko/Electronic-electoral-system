a94f444245a87b339e2de5e57d2df3f89cc3a4a2
Eduardo <112199620+EduardoManczenko@users.noreply.github.com>
2024-11-11 23:47:27 -0300

    fix: responsividade no menu da urna

--- frontend/Components/Urna/VotingKeyboard.tsx
+++ frontend/Components/Urna/VotingKeyboard.tsx
@@ -24,35 +24,39 @@ const VotingKeyboard = () => {
   };
 
   return (
-    <div className=" bg-gray-800 p-4 rounded-lg text-center max-w-[300px]">
-      <div className="bg-white text-black text-xl p-2 mb-2 h-10 rounded-md">
+    <div className="bg-gray-800 p-4 rounded-md text-center w-[100%] h-[100%] flex flex-col justify-between">
+      <div className="bg-white text-black text-xl p-2 mb-4 h-14 flex justify-center items-center rounded-md">
         {input}
       </div>
-      <div className="grid grid-cols-3 gap-2">
+
+      <div className="grid grid-cols-3 gap-2 flex-grow">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
-          <button 
-            key={num} 
-            onClick={() => handleNumberClick(num)} 
+          <button
+            key={num}
+            onClick={() => handleNumberClick(num)}
             className="bg-black text-white text-sm p-2 rounded-md"
           >
             {num}
           </button>
         ))}
-        <button 
-          onClick={handleBrancoClick} 
+
+        <button
+          onClick={handleBrancoClick}
           className="bg-gray-200 text-black text-xs p-2 col-span-2 rounded-md"
         >
           BRANCO
         </button>
-        <button 
-          onClick={handleCorrigeClick} 
+
+        <button
+          onClick={handleCorrigeClick}
           className="bg-orange-500 text-black text-xs p-2 rounded-md"
         >
           CORRIGE
         </button>
-        <button 
-          onClick={handleConfirmaClick} 
-          className="bg-green-500 text-white text-xs p-2 rounded-md"
+
+        <button
+          onClick={handleConfirmaClick}
+          className="bg-green-500 text-white text-xs p-2 col-span-2 rounded-md"
         >
           CONFIRMA
         </button>
--- frontend/Components/Urna/index.tsx
+++ frontend/Components/Urna/index.tsx
@@ -7,14 +7,19 @@ export function Urna(){
     //<Image width={800} height={700} src="/images/justica_eleitoral.png" alt="justica_eleitoral" className='rounded-md'></Image>
     return(
         <div className="bg-[#d3eed5] w-full h-screen flex flex-col items-center justify-center pt-[80px]">
-            <div className="bg-[rgb(124,218,130)] w-[80%] h-[50%] flex rounded-lg items-center justify-center gap-20">
+            <div className="bg-[rgb(124,218,130)] w-[80%] h-[50%] flex rounded-lg items-center justify-center gap-10">
                 <div className='bg-[rgb(155,214,159)] w-[60%] h-[90%] rounded-lg border-[#a4e9a8] border-4'>
 
                 <CandidateList/>
 
                 </div>
 
-                <div>
+                <div className='h-[90%] w-[30%] flex flex-col'>
+                    <div className='flex items-center justify-center gap-10 mb-2 px-4 py-2 border-green-100 bg-green-200 border-2 rounded-md'>
+                        <Image width={100} height={100} src="/images/logo_justica_eleitoral.png" alt="justica_eleitoral" className='rounded-md'></Image>
+                        <div className='text-2xl font-extrabold text-end'>JUSTIÇA ELEITORAL</div>
+                    </div>
+
                     <VotingKeyboard/>
                 </div>
             </div>
