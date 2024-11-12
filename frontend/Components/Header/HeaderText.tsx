02ee9edc087b723625279e458b4a6b9be804c509
Eduardo <112199620+EduardoManczenko@users.noreply.github.com>
2024-11-12 00:23:15 -0300

    Update HeaderText.tsx

--- frontend/Components/Header/HeaderText.tsx
+++ frontend/Components/Header/HeaderText.tsx
@@ -4,6 +4,6 @@ export default function HeaderText({
     children: React.ReactNode;
   }>){
     return(
-        <div className="bg-[#699269] px-4 py-2 text-lg text-white rounded-md cursor-pointer">{children}</div>
+        <div className="bg-[#699269] border-[2px] border-green-100 px-4 py-2 text-lg text-white rounded-md cursor-pointer">{children}</div>
     )
 }
