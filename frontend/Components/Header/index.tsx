244c7e3dc28a717c17f77ff7bcaef57dd54e1413
Eduardo <112199620+EduardoManczenko@users.noreply.github.com>
2024-11-12 22:42:19 -0300

    fix: header img

--- frontend/Components/Header/index.tsx
+++ frontend/Components/Header/index.tsx
@@ -5,11 +5,12 @@ import Image from 'next/image';
 export function Header() {
     return (
         <div className="fixed z-40 top-0 w-full h-[83px] backdrop-blur-md flex justify-center items-center border-b-2 border-green-100 bg-[rgb(124,218,130)] bg-opacity-70 px-10">
-            <div className="flex mr-[10%]">
+           
+                 <div className="">
                     <Image width={80} height={80} src="/images/logo_justica_eleitoral_2.png" alt="justica_eleitoral" />
                 </div>
 
-            <div className="flex items-center justify-between w-full max-w-[1200px]">
+            <div className="flex items-center justify-between w-full max-w-[1200px] mx-[40px]">
                 
                 <Link href='/' className="bg-[rgba(133,177,133,0.7)] hover:bg-[rgba(169,214,169,0.7)] border-r-[1px] border-b-[1px] border-green-100 px-4 py-2 h-[80px] flex flex-col items-center justify-center w-80 rounded-l-lg"><HeaderText>Urna</HeaderText></Link>
 
